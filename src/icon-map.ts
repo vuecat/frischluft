d96d5c0c1d94bf5832400be9c64d02939253d3c8
vuecat <16623919+vuecat@users.noreply.github.com>
2024-12-14 22:50:51 +0800

    fix categories logo

--- src/icon-map.ts
+++
@@ -1,6 +0,0 @@
-export const categoriesLogo= Array(
-    ['Obsidian', 'logos:obsidian-icon'],
-        ['Vue', 'logos:vue'],
-        ['Java', 'logos:java'],
-        ['Default', 'material-symbols:book-rounded']
-)
